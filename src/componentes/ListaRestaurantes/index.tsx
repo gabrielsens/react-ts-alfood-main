2090f39b41b0e71db2057176f1135963d8a29e39
Gabriel Sens <gabriel_sens@hotmail.com>
2022-07-18 23:46:12 -0300

    inicio projeto alfood

---
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
+ 
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(response => {
+        setRestaurantes([ ...restaurantes, ...response.data.results ]);
+        setProximaPagina(response.data.next);
+      })
+      .catch(erro => {
+        console.log(erro);
+      })
+  }
+
+  useEffect(() => {
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+      .then(response => {
+        setRestaurantes(response.data.results);
+        setProximaPagina(response.data.next);
+      })
+      .catch(erro => {
+        console.log(erro);
+      })
+  }, [])
+
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+      {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+      {proximaPagina && <button onClick={verMais}>Ver mais</button>}
+    </section>
+  )
+}
+
+export default ListaRestaurantes
